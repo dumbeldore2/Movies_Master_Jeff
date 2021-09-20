f452a3013cd7cdb757af3e911082920e75f2cd8c
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-09-20 19:58:58 +0200

    de knoppen werken in de MainActivity2.java

--- app/src/main/java/com/example/movies_master_jeff/MainActivity2.java
+++ app/src/main/java/com/example/movies_master_jeff/MainActivity2.java
@@ -4,10 +4,13 @@ import androidx.annotation.ColorLong;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.LinearLayout;
@@ -15,7 +18,7 @@ import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     //String initen
-    TextView textview_1;
+    TextView textview_1,textview_3,textview_4;
 
     //animatie initen
     Animation animation;
@@ -33,17 +36,22 @@ public class MainActivity2 extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.color1));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.color2));
 
         //linear connecten
         linearLayout = findViewById(R.id.linear);
 
         //texviews connecten
         textview_1 = findViewById(R.id.text_1);
+        textview_3 = findViewById(R.id.text_3);
+        textview_4 = findViewById(R.id.text_4);
 
         //funties
+        kleur_knop();
         background_ani();
+        click_textview3();
+        click_textview4();
     }
 
 
@@ -53,5 +61,38 @@ public class MainActivity2 extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(10);
         animationDrawable.setExitFadeDuration(2000);
         animationDrawable.start();
+    }
+
+    //de functie om ervoor te zorgen dat de background word veranderd van de twee buttons
+    public void kleur_knop(){
+        Intent intent = getIntent();
+        if (intent.getIntExtra("nummer",-1) == 1){
+            textview_4.setBackgroundResource(R.drawable.button_on);
+            textview_4.setTextColor(getResources().getColor(R.color.white));
+            textview_3.setBackgroundResource(R.drawable.button_off);
+            textview_3.setTextColor(getResources().getColor(R.color.color1));
+        }
+    }
+
+    public void click_textview3(){
+        textview_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("nummer",0);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void click_textview4(){
+        textview_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("nummer",1);
+                startActivity(intent);
+            }
+        });
     }
 }
