85f9e4741d7be3f1632336b5cc40938a25d1d371
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-10-27 01:43:03 +0200

    het een en ander geprobeert maar mkdir blijft niet werken

--- app/src/main/java/com/example/movies_master_jeff/MainActivity3.java
+++ app/src/main/java/com/example/movies_master_jeff/MainActivity3.java
@@ -1,24 +1,39 @@
 package com.example.movies_master_jeff;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -46,6 +61,9 @@ public class MainActivity3 extends AppCompatActivity {
 
     //foto ininten
     ImageView imageView;
+
+    //output stream ininten
+    OutputStream outputStream;
 
     //for fotos to get
     private static final int IMAGE_PICK_CODE = 1000;
@@ -112,11 +130,61 @@ public class MainActivity3 extends AppCompatActivity {
 
     public void click_text_4(){
         textView4.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
-                database.addToTabel1(getNaam(),1);
-                startActivity(intent);
+                if (!getNaam().isEmpty()){
+                    database.addToTabel1(getNaam(),1);
+                    Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
+                    File filepath = Environment.getStorageDirectory();
+                    File dir = new File(filepath.getAbsolutePath() + "/Demo/");
+
+                    if (ActivityCompat.checkSelfPermission(MainActivity3.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("true");
+                        if (dir.exists()){
+                            Toast.makeText(getApplicationContext(),"Directiry already exists",Toast.LENGTH_SHORT).show();
+                        } else {
+                            dir.mkdirs();
+                            if (dir.isDirectory()){
+                                System.out.println("lol het werkt toch");
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity3.this);
+                                String smessage = "Message : failed to creeate directory";
+                                builder.setMessage(smessage);
+                            }
+                        }
+                    }else {
+                        ActivityCompat.requestPermissions(MainActivity3.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+                        System.out.println("denieeed");
+                    }
+
+
+                    System.out.println(dir.exists());
+                    File file = new File(dir, System.currentTimeMillis()+".jpg");
+                    try {
+                        outputStream = new FileOutputStream(file);
+                        System.out.println("dees is gelukt");
+                    } catch (FileNotFoundException e) {
+                        System.out.println("dees is niet gelukt");
+                        e.printStackTrace();
+                    }
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                    Toast.makeText(getApplicationContext(),"Image save To internal",Toast.LENGTH_SHORT).show();
+                    try {
+                        outputStream.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    startActivity(intent);
+                }
             }
         });
     }
