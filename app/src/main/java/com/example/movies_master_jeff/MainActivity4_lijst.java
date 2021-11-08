16e60ca149e5f5f8bf686fd0cc4165ced410ad7b
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-11-08 19:36:58 +0100

    een extra MainActivity4.java is aangemaakt plus de llistvieuw is gevodnen en dat moet in principe lukken

---
+++ app/src/main/java/com/example/movies_master_jeff/MainActivity4_lijst.java
@@ -0,0 +1,14 @@
+package com.example.movies_master_jeff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4_lijst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity4_lijst);
+    }
+}
